714b2f5e1f457ab432975ceacdd232dedeaae0f2
Roger <roger.santos36@gmail.com>
2021-09-26 16:03:58 -0300

    desafio1 codelandia

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '50px',
+  duration: 800,
+  reset: false
+});
+
+ScrollReveal().reveal('.article_content', { delay: 100 });
+ScrollReveal().reveal('.article_info', { delay: 100 });
+ScrollReveal().reveal('.article_news a', { delay: 100 });
+ScrollReveal().reveal('.article_news p', { delay: 100 });
